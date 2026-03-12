cc74acce24db7a45c737d668aaa8a5daff7ffbc2
Karim-Rahat <rah_at@outlook.com>
2026-03-12 09:09:26 +0000

    Refactor horizontal navigation: comment out 'coming soon' link, add eZWay PPV link, and adjust video module visibility

--- Modules/Frontend/Resources/views/components/partials/horizontal-nav.blade.php
+++ Modules/Frontend/Resources/views/components/partials/horizontal-nav.blade.php
@@ -64,19 +64,32 @@
         </a>
       </li>
       @endif -->
-      <li class="nav-item">
+      <!-- <li class="nav-item">
         <a class="nav-link {{ request()->routeIs('comingsoon') ? 'active text-primary' : '' }}"  href="{{ route('comingsoon') }}">
           <span class="item-name">{{__('frontend.coming_soon')}}</span>
         </a>
-      </li>
+      </li> -->
+
       @if(isenablemodule('livetv'))
       <li class="nav-item">
         <a class="nav-link {{ request()->routeIs('livetv') ? 'active text-primary' : '' }}"  href="{{route('livetv')}}">
           <span class="item-name">{{__('frontend.livetv')}}</span>
         </a>
       </li>
+      
       @endif
-
+      @if(isenablemodule('video'))
+      <li class="nav-item">
+        <a class="nav-link"  href="{{ route('videos') }}">
+          <span class="item-name">{{__('frontend.video')}}</span>
+        </a>
+      </li>
+      @endif 
+       <li class="nav-item">
+        <a class="nav-link {{ request()->routeIs('livetv') ? 'active text-primary' : '' }}" target="_blank"  href="https://ppv.ezway.tv/">
+          <span class="item-name" style="text-transform: none;">eZWay PPV</span>
+        </a>
+      </li>
     </ul>
   </div>
   <!-- container-fluid.// -->
