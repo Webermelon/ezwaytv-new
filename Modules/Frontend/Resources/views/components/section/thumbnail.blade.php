40608502d229f665ca486c9af86919173435eaa1
Karim-Rahat <rah_at@outlook.com>
2026-03-26 11:55:55 +0000

    feat: Update video player setup for iPhone compatibility and change file permissions for multiple .gitignore files

--- Modules/Frontend/Resources/views/components/section/thumbnail.blade.php
+++ Modules/Frontend/Resources/views/components/section/thumbnail.blade.php
@@ -15,9 +15,19 @@
         <script src="{{ asset('js/videojs/videojs.ima.min.js') }}"></script>
         <link href="{{ asset('css/videojs.ima.css') }}" rel="stylesheet">
 
+        @php
+            $userAgent = request()->header('User-Agent');
+            $isIphone = stripos($userAgent, 'iPhone') !== false;
+        @endphp
+
         <div class="video-player">
             <video id="videoPlayer" class="video-js vjs-default-skin vjs-ima" controls width="560" height="315" muted
                 poster="{{ $thumbnail_image }}" data-type="{{ $type }}"
+
+                @unless($isIphone)
+                    data-setup='{"muted": true}'
+                @endunless
+
                 content-video-type="{{ $content_video_type }}"
                 data-continue-watch="{{ isset($continue_watch) && $continue_watch ? 'true' : 'false' }}"
                 data-movie-access="{{ $dataAccess ?? '' }}" data-plan-id="{{ $plan_id ?? '' }}"
